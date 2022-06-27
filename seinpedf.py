ba5b6edcca5e98c87d4997d3fa6ad09e0446bf1b
Edu Rosúa <eduardorosuae@gmail.com>
2022-06-27 22:59:24 +0200

    FEAT: se generan un numero de paginas indicado por parametro

--- seinpedf.py
+++ seinpedf.py
@@ -4,6 +4,7 @@ from tkinter.filedialog import askopenfilename
 from fitz import fitz, Rect
 from pathlib import Path
 from PyPDF2 import PdfFileReader
+import math
 
 def inicio():
     Tk().withdraw() #|Cuadro de dialogo para especificar ruta del informe base
@@ -14,7 +15,7 @@ def inicio():
     num_referencia = capturaNumReferencia(pdf_original)
     list_imgs = [
     Path(Path.cwd(),"ejemplo/Fotos/20220131110845.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110845.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110852.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110905.jpg"),
@@ -25,27 +26,29 @@ def inicio():
     subdivision = 4
     listas_imgs = [list_imgs[i:i + subdivision] for i in range(0, len(list_imgs), subdivision)]
 
-    agregarPagina(pdf_original, pdf_plantilla, pdf_aux)
-    if Path(Path.cwd(),"pruebaImagenes.pdf").exists():
-        archivo = Path(Path.cwd(),"pruebaImagenes.pdf")
-    else:
-        archivo = pdf_aux
+    # if Path(Path.cwd(),"pruebaImagenes.pdf").exists():
+    #     archivo = Path(Path.cwd(),"pruebaImagenes.pdf")
+    # else:
+    #     archivo = pdf_aux
     
+    # agregarPagina(pdf_original, pdf_plantilla, pdf_aux, math.floor(len(list_imgs)/subdivision))
+    agregarPagina(pdf_original, pdf_plantilla, pdf_aux, 4)
     for sublista_img in listas_imgs:
-        generaPdfConImgs(archivo, sublista_img)
+        generaPdfConImgs(pdf_aux, sublista_img)
     
     pdf_aux.unlink()
 
-def agregarPagina(pdf_ruta: Path, pdf_plantilla: Path, pdf_aux: Path): #Agrega una nueva pagina con el formato base para añadir fotos
+def agregarPagina(pdf_ruta: Path, pdf_plantilla: Path, pdf_aux: Path, num_paginas: int): #Agrega una nueva pagina con el formato base para añadir fotos
     pdfOriginal = fitz.open(pdf_ruta)
-    pdfNuevo = fitz.open(pdf_plantilla)
-    
-    pdfOriginal.insert_pdf(pdfNuevo)
-    nuevaPagina = pdfOriginal.load_page(-1)
-    nuevaPagina.set_rotation(0)
-    if not nuevaPagina.is_wrapped:
-        nuevaPagina.wrap_contents()
-    pdfOriginal.save(pdf_aux) #Nombre archivo de salidaImagen a inserta
+    for num_paginas in range(0, num_paginas):
+        pdfNuevo = fitz.open(pdf_plantilla)
+        
+        pdfOriginal.insert_pdf(pdfNuevo)
+        ultima_pagina = pdfOriginal.load_page(-1)
+        ultima_pagina.set_rotation(0)
+        if not ultima_pagina.is_wrapped:
+            ultima_pagina.wrap_contents()
+        pdfOriginal.save(pdf_aux) #Nombre archivo de salidaImagen a inserta
 
 def capturaNumReferencia(pdf_original: Path):
     with open(pdf_original, 'rb') as f:
@@ -55,18 +58,18 @@ def capturaNumReferencia(pdf_original: Path):
 
 ######PRUEBAS DE IMPORTACION Y ESCALADO DE IMAGENES###############
 
-def generaPdfConImgs(archivo, list_imgs):
+def generaPdfConImgs(pdf_aux, list_imgs):
     posicionXmin = 50
     posicionYmin = 225
     posicionXmax = 275 
     posicionYmax = 400
-    pos = 0
-    pdf_modificado = fitz.open(archivo) #PDF en el que se insertara la imagen
-
-    for img in list_imgs:
+    pdf_modificado = fitz.open(pdf_aux) #PDF en el que se insertara la imagen
+    pdf_ultima_pagina = pdf_modificado.load_page(-1) #Pagina en la que se insertara la imagen
+    
+    for pos, img in enumerate(list_imgs):
+        print(pos)
+        print(img)
         imagen_a_cargar = Path(img).read_bytes()
-        posicion_img = fitz.Rect(int(posicionXmin),int(posicionYmin), int(posicionXmax), int(posicionYmax)) #Posicion y tamaño de la imagen
-        pdf_ultima_pagina = pdf_modificado.load_page(-1) #Pagina en la que se insertara la imagen
 
         if pos == 0:
             posicionXmin = 50
@@ -89,14 +92,16 @@ def generaPdfConImgs(archivo, list_imgs):
             posicionYmin = 550
             posicionXmax = 545 
             posicionYmax = 725
-        pos += 1
 
+        posicion_img = fitz.Rect(int(posicionXmin),int(posicionYmin), int(posicionXmax), int(posicionYmax)) #Posicion y tamaño de la imagen
         pdf_ultima_pagina.insert_image(posicion_img, stream = imagen_a_cargar, keep_proportion = False) #Inserción de imagen
     
-    if Path(Path.cwd(),"pruebaImagenes.pdf").exists():
-        pdf_modificado.saveIncr()
-    else:
-        pdf_modificado.save(Path(Path.cwd(),"pruebaImagenes.pdf"))
+    # Revisar saveIncr ya que no se está sobrescribiendo
+    # if Path(Path.cwd(),"pruebaImagenes.pdf").exists():
+    #     pdf_modificado.saveIncr()
+    # else:
+
+    pdf_modificado.save(Path(Path.cwd(),"pruebaImagenes.pdf"))
 
 inicio()
     
