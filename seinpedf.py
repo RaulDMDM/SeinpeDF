351705c22ab50ddded4e1367bf01e848ac1ec51e
Edu Rosúa <eduardorosuae@gmail.com>
2022-06-27 19:59:47 +0200

    FEAT: Refactorización en funciones y se pintan imagenes en base a una lista dada

--- seinpedf.py
+++ seinpedf.py
@@ -5,57 +5,82 @@ import fitz
 from pathlib import Path
 from PyPDF2 import PdfFileReader
 
-pdf_img_insertada = Path(Path.cwd(),"seinpeDF/pruebaAdd.pdf")
-pdf_modificado = Path(Path.cwd(), 'seinpeDF/ejemplo/marcaDeAgua.pdf')
+def inicio():
+    Tk().withdraw()                 #|Cuadro de dialogo para especificar ruta del informe base
+    pdf_original = askopenfilename()
+    pdf_plantilla = Path(Path.cwd(), "ejemplo/marcaDeAgua.pdf")
+    pdf_carga_img = Path(Path.cwd(),"pruebaAdd.pdf")
 
-def agregarPagina(): #Agrega una nueva pagina con el formato base para añadir fotos
-    pdfOriginal = fitz.open(rutaInforme)
-    pdfNuevo = fitz.open(pdf_modificado)
+    num_referencia = capturaNumReferencia(pdf_original)
+    list_imgs = [Path(Path.cwd(),"ejemplo/Fotos/20220131110845.jpg"),Path(Path.cwd(),"ejemplo/Fotos/20220131110852.jpg"),Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg")]
+
+    agregarPagina(pdf_original, pdf_plantilla, pdf_carga_img)
+    generaPdfConImgs(pdf_carga_img, list_imgs)
+
+def agregarPagina(pdf_ruta: Path, pdf_plantilla: Path, pdf_carga_img: Path): #Agrega una nueva pagina con el formato base para añadir fotos
+    pdfOriginal = fitz.open(pdf_ruta)
+    pdfNuevo = fitz.open(pdf_plantilla)
     
     pdfOriginal.insert_pdf(pdfNuevo)
-    nuevaPagina = pdfOriginal[len(pdfOriginal) - 1]
+    nuevaPagina = pdfOriginal.load_page(-1)
     nuevaPagina.set_rotation(0)
     if not nuevaPagina.is_wrapped:
         nuevaPagina.wrap_contents()
-    # pdfOriginal.save("pruebaAdd.pdf")
-    pdfOriginal.save(pdf_img_insertada) #Nombre archivo de salidaImagen a inserta
-
+    pdfOriginal.save(pdf_carga_img) #Nombre archivo de salidaImagen a inserta
 
 def capturaNumReferencia(pdf_original: Path):
     with open(pdf_original, 'rb') as f:
-        pdf = PdfFileReader(f)
+        pdf = PdfFileReader(f, strict = False)
         pagina = pdf.getPage(0)
         return pagina.extractText()[-10:]
 
-pdf_original = askopenfilename()
-
-Tk().withdraw()                 #|Cuadro de dialogo para especificar ruta del informe base
-rutaInforme = askopenfilename() #|
-numReferencia = capturaNumReferencia()
 #pdf = parser.from_file(rutaInforme) #Lectura del PDF de informe base
 
 #text = pdf['content'] #Interpretacion del texto de informe base
 
 #print(text)
 
-agregarPagina()
+# agregarPagina(pdf_ruta)
 
 ######PRUEBAS DE IMPORTACION Y ESCALADO DE IMAGENES###############
 
-archivoSalida = Path(Path.cwd(),"seinpeDF/pruebaImagenes.pdf") #Nombre archivo de salidaImagen a insertar
+def generaPdfConImgs(pdf_carga_img, list_imgs):
+    pdf_salida = Path(Path.cwd(),"pruebaImagenes.pdf") #Nombre archivo de salidaImagen a insertar
+    posicionXmin = 50 #
+    posicionYmin = 225
+    posicionXmax = 275 
+    posicionYmax = 400
+    # imagenPrueba = open(Path(".\\ejemplo\\naranjito.png"), 'rb').read() 
+    # posiciones_img = 
+    # 50, 225, 275, 400 - 
+    # 320, 225, 545, 400 - 
+    # 50, 550, 275, 725 -
+    # 320, 550, 545, 725
+    fila = 0
+    columna= 0
+    for img in list_imgs:
+        imagen_a_cargar = Path(img).read_bytes()
+        posicion_img = fitz.IRect(int(posicionXmin),int(posicionYmin), int(posicionXmax), int(posicionYmax)) #Posicion y tamaño de la imagen
+        pdf_modificado = fitz.open(pdf_carga_img) #PDF en el que se insertara la imagen
+        nueva_pagina_insertada = pdf_modificado.load_page(-1) #Pagina en la que se insertara la imagen
+        columna += 1
+        if columna == 2:
+            columna = 0
+            posicionXmin = 50
+            posicionXmax = 275
+            posicionYmin += 225
+            posicionYmax += 325
+        else:
+            posicionXmin += 270
+            posicionXmax += 270
+            posicionYmin = 225
+            posicionYmax = 400
 
-# imagenPrueba = open(Path(".\\ejemplo\\naranjito.png"), 'rb').read() 
-imagenPrueba = Path(Path.cwd(), "seinpeDF/ejemplo/naranjito.png").read_bytes()
+        nueva_pagina_insertada.insert_image(posicion_img, stream = imagen_a_cargar, keep_proportion = False) #Inserción de imagen
 
-imagenPosicion = fitz.IRect(0,0,50,50) #Posicion y tamaño de la imagen
+    pdf_modificado.save(pdf_salida)
 
-archivo = fitz.open(pdf_img_insertada) #PDF en el que se insertara la imagen
-paginaInsercion = archivo[len(archivo) - 1] #Pagina en la que se insertara la imagen
-
-paginaInsercion.insert_image(imagenPosicion, stream = imagenPrueba) #Inserción de imagen
-
-archivo.save(archivoSalida)
-
+inicio()
     
 
 
