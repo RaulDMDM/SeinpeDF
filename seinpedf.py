ddd1f2f19054f2dd53ac793019419000e52597a1
RaulDMDM <rauldmdmcode@gmail.com>
2022-06-28 15:02:29 +0200

    Feat: Los textos e imagenes se colocan correctamente en base a las listas establecidas. Falta crear la GUI e incorporar el numero de referencia al final de cada pagina.

--- seinpedf.py
+++ seinpedf.py
@@ -1,18 +1,22 @@
 # from tika import parser
+from argparse import FileType
 import builtins
-from fileinput import close
+from fileinput import close, filename
 from importlib.resources import path
-from re import I
-from tkinter import Tk
+from tkinter import *
+from tkinter import Tk, ttk
+from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showinfo
 from fitz import fitz, Rect
 from pathlib import Path
 from PyPDF2 import PdfFileReader
 import math
 
 def inicio():
+    
     Tk().withdraw() #|Cuadro de dialogo para especificar ruta del informe base
-    pdf_original = askopenfilename()
+    pdf_original = askopenfilename(title="Selecciona el informe Siebel", filetypes=[("PDF", "*.pdf")])
     pdf_peritacion = Path(Path.cwd(), "ejemplo/peritacionGT.pdf")
     pdf_plantilla = Path(Path.cwd(), "ejemplo/marcaDeAgua.pdf")
     pdf_aux = Path(Path.cwd(),"pdfAux.pdf")
@@ -20,23 +24,52 @@ def inicio():
     num_referencia = capturaNumReferencia(pdf_original)
     list_imgs = [
     Path(Path.cwd(),"ejemplo/Fotos/20220131110845.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110852.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110905.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110907.jpg"),
     Path(Path.cwd(),"ejemplo/Fotos/20220131110918.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110845.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110852.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110858.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110905.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110907.jpg"),
-    Path(Path.cwd(),"ejemplo/Fotos/20220131110918.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110922.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110923.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110928.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110931.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131110935.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131111043.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131111207.jpg"),
+    Path(Path.cwd(),"ejemplo/Fotos/20220131111134.jpg"),
     ]
-
-    subdivision = 4
-    listas_imgs = [list_imgs[i:i + subdivision] for i in range(0, len(list_imgs), subdivision)]
+    list_tit = [
+        "Texto 1",
+        "Texto 2",
+        "Texto 3",
+        "Texto 4",
+        "Texto 5",
+        "Texto 6",
+        "Texto 7",
+        "Texto 8",
+        "Texto 9",
+        "Texto 10",
+        "Texto 11",
+        "Texto 12",
+        "Texto 13",
+        "Texto 14"]
+    list_desc = [
+        "Texto 1",
+        "Texto 2",
+        "Texto 3",
+        "Texto 4",
+        "Texto 5",
+        "Texto 6",
+        "Texto 7",
+        "Texto 8",
+        "Texto 9",
+        "Texto 10",
+        "Texto 11",
+        "Texto 12",
+        "Texto 13",
+        "Texto 14"]
+    #subdivision = 4
+    #listas_imgs = [list_imgs[i:i + subdivision] for i in range(0, len(list_imgs), subdivision)]
     
     generar_num_paginas = int((len(list_imgs)/4) + 1) #Indica el numero de paginas que tiene que generar en base a la lista de Imagenes
 
@@ -47,8 +80,7 @@ def inicio():
     
     # agregarPagina(pdf_original, pdf_plantilla, pdf_aux, math.floor(len(list_imgs)/subdivision))
     agregarPaginas(pdf_original, pdf_plantilla,pdf_peritacion, pdf_aux, generar_num_paginas)
-    for sublista_img in listas_imgs:
-        generaPdfConImgs(pdf_aux, list_imgs, generar_num_paginas)
+    generaPdfConImgs(pdf_aux, list_imgs,list_tit, list_desc, generar_num_paginas)
     
     pdf_aux.unlink()
 def agregarPaginas(pdf_ruta: Path, pdf_plantilla: Path, pdf_peritacion: Path, pdf_aux: Path, num_paginas: int): #Agrega una nueva pagina con el formato base para añadir fotos
@@ -70,13 +102,12 @@ def capturaNumReferencia(pdf_original: Path):
         pagina = pdf.getPage(0)
         return pagina.extractText()[-10:]
 
-######PRUEBAS DE IMPORTACION Y ESCALADO DE IMAGENES###############
+def generaPdfConImgs(pdf_aux, list_imgs, list_tit, list_desc, paginas_img):
 
-def generaPdfConImgs(pdf_aux, list_imgs, paginas_img):
     posicionXmin = 50
-    posicionYmin = 225
+    posicionYmin = 180
     posicionXmax = 275 
-    posicionYmax = 400
+    posicionYmax = 355
     i = 0
     pdf_modificado = fitz.open(pdf_aux) #PDF en el que se insertara la imagen
     current_page = len(pdf_modificado) - paginas_img #Calcula la longitud del pdf con las plantillas para imagenes y resta el numero de paginas que corresponden a esas plantillas
@@ -90,21 +121,27 @@ def generaPdfConImgs(pdf_aux, list_imgs, paginas_img):
 
         if i == 0:
             posicionXmin = 50
-            posicionYmin = 225
+            posicionYmin = 180
             posicionXmax = 275 
-            posicionYmax = 400
+            posicionYmax = 355
+            pdf_ultima_pagina.insert_textbox((50,75,275,255), list_tit[pos], align = 1, fontsize = 13, fontname = "hebo")
+            pdf_ultima_pagina.insert_textbox((50,120,275,255), list_desc[pos], align = 1, fontsize = 10, fontname = "helv")
             i += 1
         elif i == 1:
             posicionXmin = 320
-            posicionYmin = 225
+            posicionYmin = 180
             posicionXmax = 545 
-            posicionYmax = 400
+            posicionYmax = 355
+            pdf_ultima_pagina.insert_textbox((320,75,545,255), list_tit[pos], align = 1, fontsize = 13, fontname = "hebo")
+            pdf_ultima_pagina.insert_textbox((320,120,545,255), list_desc[pos], align = 1, fontsize = 10, fontname = "helv")
             i += 1
         elif i == 2:
             posicionXmin = 50
             posicionYmin = 550
             posicionXmax = 275 
             posicionYmax = 725
+            pdf_ultima_pagina.insert_textbox((50,445,275,625), list_tit[pos], align = 1, fontsize = 13, fontname = "hebo")
+            pdf_ultima_pagina.insert_textbox((50,490,275,625), list_desc[pos], align = 1, fontsize = 10, fontname = "helv")
             i += 1
         elif i == 3:
             i = 0
@@ -112,18 +149,22 @@ def generaPdfConImgs(pdf_aux, list_imgs, paginas_img):
             posicionYmin = 550
             posicionXmax = 545 
             posicionYmax = 725
+            pdf_ultima_pagina.insert_textbox((320,445,545,625), list_tit[pos], align = 1, fontsize = 13, fontname = "hebo")
+            pdf_ultima_pagina.insert_textbox((320,490,545,625), list_desc[pos], align = 1, fontsize = 10, fontname = "helv")
             current_page += 1
-
+   
         posicion_img = fitz.Rect(int(posicionXmin),int(posicionYmin), int(posicionXmax), int(posicionYmax)) #Posicion y tamaño de la imagen
         pdf_ultima_pagina.insert_image(posicion_img, stream = imagen_a_cargar, keep_proportion = False) #Inserción de imagen
+        
     
+        
     # Revisar saveIncr ya que no se está sobrescribiendo
     # if Path(Path.cwd(),"pruebaImagenes.pdf").exists():
     #     pdf_modificado.saveIncr()
     # else:
 
     pdf_modificado.save(Path(Path.cwd(),"pruebaImagenes.pdf"))
-
+ 
 inicio()
     
 
